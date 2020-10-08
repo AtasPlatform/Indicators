bb986b90f6e39f949e039629d2e5090e9d7ef1d1
AtasPlatform <support@atas.net>
2020-10-08 14:02:14 +0700

    Assembly info updated.

--- Technical/Properties/AssemblyInfo.cs
+++ Technical/Properties/AssemblyInfo.cs
@@ -7,36 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Technical Indicators")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("ATAS LTD")]
-[assembly: AssemblyProduct("ATAS Platform")]
-[assembly: AssemblyCopyright("Copyright © 2011-2020")]
-[assembly: AssemblyTrademark("OrderFlowTrading.net")]
-[assembly: AssemblyCulture("")]
-
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("38926823-8d53-4ed1-8d11-a88a82ceba34")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("5.7.8.7")]
-//[assembly: AssemblyFileVersion("4.0.0")]
