7f4eab132034cd4c1f8ffa97c2a644ffc61333e8
Stig <Stan422@gmail.com>
2020-10-12 16:47:35 +0700

    --MFI real-time fix --MFI sum fix

--- Technical/MFI.cs
+++ Technical/MFI.cs
@@ -1,155 +1,148 @@
 ﻿namespace ATAS.Indicators.Technical
 {
-	using System.Collections.Generic;
+	using System;
 	using System.ComponentModel;
-	using System.ComponentModel.DataAnnotations;
-	using System.Linq;
-	using System.Windows.Media;
+    using System.ComponentModel.DataAnnotations;
+    using System.Windows.Media;
 
-	using ATAS.Indicators.Technical.Properties;
+    using ATAS.Indicators.Technical.Properties;
 
-	[DisplayName("MFI")]
-	public class MFI : Indicator
-	{
-		#region Fields
+    [DisplayName("MFI")]
+    public class MFI : Indicator
+    {
+        #region Fields
 
-		private readonly LineSeries _overbought = new LineSeries("Overbought");
-		private readonly LineSeries _oversold = new LineSeries("Oversold");
+        private readonly LineSeries _overbought = new LineSeries("Overbought");
+        private readonly LineSeries _oversold = new LineSeries("Oversold");
+        private ValueDataSeries _series = new ValueDataSeries("MFI");
 
-		private List<decimal> _flows = new List<decimal>();
-		private int _period;
-		private int _prevBar;
-		private decimal _previousTypical;
+        private int _period;
+        private int _previousBar;
+        private decimal _previousTypical;
+        private  ValueDataSeries _positiveFlow = new ValueDataSeries("PosFlow");
+        private  ValueDataSeries _negativeFlow = new ValueDataSeries("NegFlow");
 
-		#endregion
+        #endregion
 
-		#region Properties
+        #region Properties
 
-		[Parameter]
-		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Common", Order = 20)]
-		public int Period
-		{
-			get => _period;
-			set
-			{
-				if (value <= 0)
-					return;
+        [Parameter]
+        [Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Common", Order = 20)]
+        public int Period
+        {
+            get => _period;
+            set
+            {
+                if (value <= 0)
+                    return;
 
-				_period = value;
-				_previousTypical = -1;
-				RecalculateValues();
-			}
-		}
+                _period = value;
+                _previousTypical = -1;
+                RecalculateValues();
+            }
+        }
 
-		[Parameter]
-		[Display(ResourceType = typeof(Resources), Name = "Overbought", GroupName = "Common", Order = 10)]
-		public decimal Overbought
-		{
-			get => _overbought.Value;
-			set
-			{
-				if (value < _oversold.Value)
-					return;
+        [Parameter]
+        [Display(ResourceType = typeof(Resources), Name = "Overbought", GroupName = "Common", Order = 10)]
+        public decimal Overbought
+        {
+            get => _overbought.Value;
+            set
+            {
+                if (value < _oversold.Value)
+                    return;
 
-				_overbought.Value = value;
-			}
-		}
+                _overbought.Value = value;
+            }
+        }
 
-		[Parameter]
-		[Display(ResourceType = typeof(Resources), Name = "Oversold", GroupName = "Common", Order = 20)]
-		public decimal Oversold
-		{
-			get => _oversold.Value;
-			set
-			{
-				if (value > _overbought.Value)
-					return;
+        [Parameter]
+        [Display(ResourceType = typeof(Resources), Name = "Oversold", GroupName = "Common", Order = 20)]
+        public decimal Oversold
+        {
+            get => _oversold.Value;
+            set
+            {
+                if (value > _overbought.Value)
+                    return;
 
-				_oversold.Value = value;
-			}
-		}
+                _oversold.Value = value;
+            }
+        }
 
-		#endregion
+        #endregion
 
-		#region ctor
+        #region ctor
 
-		public MFI()
-			: base(true)
-		{
-			Panel = IndicatorDataProvider.NewPanel;
-			_period = 14;
-			_prevBar = -1;
+        public MFI()
+            : base(true)
+        {
+            Panel = IndicatorDataProvider.NewPanel;
+            _period = 14;
+            _previousBar = -1;
+            _overbought.Color = _oversold.Color = Colors.Green;
+            _overbought.Value = 80;
+            _oversold.Value = 20;
 
-			_overbought.Color = _oversold.Color = Colors.Green;
-			_overbought.Value = 80;
-			_oversold.Value = 20;
+            _series.ShowZeroValue = false;
+            DataSeries[0] = _series;
 
-			DataSeries.Clear();
+            LineSeries.Add(_overbought);
+            LineSeries.Add(_oversold);
+        }
 
-			var mfiSeries = new ValueDataSeries("MFI")
-			{
-				ShowZeroValue = false
-			};
-			DataSeries.Add(mfiSeries);
+        #endregion
 
-			LineSeries.Add(_overbought);
-			LineSeries.Add(_oversold);
-		}
+        #region Protected methods
 
-		#endregion
+        protected override void OnCalculate(int bar, decimal value)
+        {
+            var currentCandle = GetCandle(bar);
+            var typical = (currentCandle.High + currentCandle.Low + currentCandle.Close) / 3.0m;
 
-		#region Protected methods
+            if (bar == 0)
+            {
+                _previousTypical = typical;
+                _series.SetPointOfEndLine(Period);
+            }
 
-		protected override void OnCalculate(int bar, decimal value)
-		{
-			var currentCandle = GetCandle(bar);
-			var typical = (currentCandle.High + currentCandle.Low + currentCandle.Close) / 3.0m;
+            var moneyFlow = typical * currentCandle.Volume;
 
-			if (bar == 0)
-			{
-				_flows.Clear();
-				_previousTypical = typical;
 
-				var series = (ValueDataSeries)DataSeries[0];
-				series.SetPointOfEndLine(_period - 2);
-			}
+            if (typical > _previousTypical)
+            {
+                _positiveFlow[bar] = moneyFlow;
+            }
+            else
+            {
+                _negativeFlow[bar] = moneyFlow;
+            }
 
-			var moneyFlow = typical * currentCandle.Volume;
 
-			if (typical < _previousTypical)
-				moneyFlow = -moneyFlow;
+            if (bar < Period)
+            {
+                _series[bar] = 0m;
+                return;
+            }
 
-			if (bar != _prevBar)
-			{
-				if (_flows.Count >= Period)
-					_flows = _flows.Skip(1).ToList();
-			}
-			else
-				_flows.RemoveAt(_flows.Count - 1);
+            var positiveFlow = _positiveFlow.CalcSum(Period, Math.Max(bar-Period,0));
+            var negativeFlow = _negativeFlow.CalcSum(Period, Math.Max(bar - Period, 0));
 
-			_flows.Add(moneyFlow);
+            if (negativeFlow == 0.0m)
+	            _series[bar] = 100.0m;
+            else
+            {
+                var moneyRatio = positiveFlow / negativeFlow;
+                _series[bar] = 100.0m - 100.0m / (1.0m + moneyRatio);
+            }
 
-			if (_flows.Count < Period)
-			{
-				DataSeries[0][bar] = 0m;
-				return;
-			}
+            if (bar != _previousBar)
+            {
+	            _previousTypical = typical;
+	            _previousBar = bar;
+            }
+        }
 
-			var positiveFlow = _flows.Where(x => x > 0).Sum();
-			var negativeFlow = _flows.Where(x => x < 0).Sum(x => -x);
-
-			if (negativeFlow == 0.0m)
-				DataSeries[0][bar] = 100.0m;
-			else
-			{
-				var moneyRatio = positiveFlow / negativeFlow;
-				DataSeries[0][bar] = 100.0m - 100.0m / (1.0m + moneyRatio);
-			}
-
-			_previousTypical = typical;
-			_prevBar = bar;
-		}
-
-		#endregion
-	}
+        #endregion
+    }
 }
