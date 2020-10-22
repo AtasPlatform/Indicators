0ce6496dea13b07eed9a8ac095048973a19a400e
Stig <Stan422@gmail.com>
2020-10-22 15:30:28 +0700

    --Alligator shift options added

--- Technical/Alligator.cs
+++ Technical/Alligator.cs
@@ -17,6 +17,10 @@ namespace ATAS.Indicators.Technical
 		private readonly SMMA _lips = new SMMA();
 		private readonly SMMA _teeth = new SMMA();
 
+		private int _jawShift;
+		private int _lipsShift;
+		private int _teethShift;
+
 		#endregion
 
 		#region Properties
@@ -32,6 +36,17 @@ namespace ATAS.Indicators.Technical
 			}
 		}
 
+		[DisplayName("Jaw shift")]
+		public int JawShift
+		{
+			get => _jawShift;
+			set
+			{
+				_jawShift = value;
+				RecalculateValues();
+			}
+		}
+
 		[DisplayName("2. Teeth Period")]
 		public int TeethPeriod
 		{
@@ -39,6 +54,17 @@ namespace ATAS.Indicators.Technical
 			set
 			{
 				_teeth.Period = Math.Max(1, value);
+				RecalculateValues();
+			}
+		}
+
+		[DisplayName("Teeth Shift")]
+		public int TeethShift
+		{
+			get => _teethShift;
+			set
+			{
+				_teethShift = value;
 				RecalculateValues();
 			}
 		}
@@ -54,25 +80,43 @@ namespace ATAS.Indicators.Technical
 			}
 		}
 
+		[DisplayName("Lips shift")]
+		public int LipsShift
+		{
+			get => _lipsShift;
+			set
+			{
+				_lipsShift = value;
+				RecalculateValues();
+			}
+		}
+
 		#endregion
 
 		#region ctor
 
 		public Alligator()
 		{
+			_jawShift = 8;
+			_teethShift = 5;
+			_lipsShift = 3;
+
 			((ValueDataSeries)DataSeries[0]).Name = "1 Jaw";
 			((ValueDataSeries)DataSeries[0]).VisualType = VisualMode.Line;
+			((ValueDataSeries)DataSeries[0]).ShowZeroValue = false;
 			((ValueDataSeries)DataSeries[0]).Color = Colors.Blue;
 
 			DataSeries.Add(new ValueDataSeries("2 Teeth")
 			{
 				VisualType = VisualMode.Line,
+				ShowZeroValue = false,
 				Color = Colors.Red
 			});
 
 			DataSeries.Add(new ValueDataSeries("3 Lips")
 			{
 				VisualType = VisualMode.Line,
+				ShowZeroValue = false,
 				Color = Colors.Green
 			});
 
@@ -87,21 +131,38 @@ namespace ATAS.Indicators.Technical
 
 		protected override void OnCalculate(int bar, decimal value)
 		{
+			if (bar == 0)
+			{
+				((ValueDataSeries)DataSeries[0]).SetPointOfEndLine(LastVisibleBarNumber + _jawShift);
+				((ValueDataSeries)DataSeries[1]).SetPointOfEndLine(LastVisibleBarNumber + _teethShift);
+				((ValueDataSeries)DataSeries[2]).SetPointOfEndLine(LastVisibleBarNumber + _lipsShift);
+			}
+
 			var average = (GetCandle(bar).Low + GetCandle(bar).High) / 2;
-			if (bar < 8)
+
+			if (bar < _jawShift)
 				this[bar] = average;
 			else
-				this[bar] = _jaw.Calculate(bar - 8, (GetCandle(bar - 8).Low + GetCandle(bar - 8).High) / 2);
+			{
+				if (bar - _jawShift <= LastVisibleBarNumber)
+					this[bar] = _jaw.Calculate(bar - _jawShift, (GetCandle(bar - _jawShift).Low + GetCandle(bar - _jawShift).High) / 2);
+			}
 
-			if (bar < 5)
+			if (bar < _teethShift)
 				DataSeries[1][bar] = average;
 			else
-				DataSeries[1][bar] = _teeth.Calculate(bar - 5, (GetCandle(bar - 5).Low + GetCandle(bar - 5).High) / 2);
+			{
+				if (bar - _teethShift <= LastVisibleBarNumber)
+					DataSeries[1][bar] = _teeth.Calculate(bar - _teethShift, (GetCandle(bar - _teethShift).Low + GetCandle(bar - _teethShift).High) / 2);
+			}
 
-			if (bar < 3)
+			if (bar < _lipsShift)
 				DataSeries[2][bar] = average;
 			else
-				DataSeries[2][bar] = _lips.Calculate(bar - 3, (GetCandle(bar - 3).Low + GetCandle(bar - 3).High) / 2);
+			{
+				if (bar - _lipsShift <= LastVisibleBarNumber)
+					DataSeries[2][bar] = _lips.Calculate(bar - _lipsShift, (GetCandle(bar - _lipsShift).Low + GetCandle(bar - _lipsShift).High) / 2);
+			}
 		}
 
 		#endregion
