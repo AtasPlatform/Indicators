3e5818633fdb50a607c0a0f32da02f0a4649e991
Stig <Stan422@gmail.com>
2020-10-23 17:35:26 +0700

    --QQE indicator added

--- Technical/QQE.cs
+++ Technical/QQE.cs
@@ -1,112 +1,168 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ATAS.Indicators.Technical
+﻿namespace ATAS.Indicators.Technical
 {
-    using System.ComponentModel;
-    using System.ComponentModel.DataAnnotations;
+	using System;
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+	using System.Windows.Media;
 
-    using ATAS.Indicators.Technical.Properties;
+	using ATAS.Indicators.Technical.Properties;
 
-    [DisplayName("Qualitative Quantitative Estimation")]
-    public class QQE : Indicator
-    {
-        private readonly RSI _rsi = new RSI();
-        private readonly EMA _ema = new EMA();
-        private readonly EMA _emaWilders = new EMA();
-        private readonly EMA _emaAtrRsi = new EMA();
-        private readonly ValueDataSeries _trLevelSlow = new ValueDataSeries("LevelSlow");
-        private readonly ValueDataSeries _rsiMa = new ValueDataSeries("RsiMa");
+	[DisplayName("Qualitative Quantitative Estimation")]
+	public class QQE : Indicator
+	{
+		#region Static and constants
 
+		private const decimal QQEmultiplier = 4.236m;
 
-        [Display(ResourceType = typeof(Resources), Name = "RSI", GroupName = "Common")]
-        public int RsiPeriod
-        {
-            get => _rsi.Period;
-            set
-            {
-                if (value <= 0)
-                    return;
+		#endregion
 
-                _rsi.Period = value;
-                _emaWilders.Period = value * 2 - 1;
-                _emaAtrRsi.Period = value * 2 - 1;
-                RecalculateValues();
-            }
-        }
+		#region Fields
 
-        [Display(ResourceType = typeof(Resources), Name = "SlowFactor", GroupName = "Common")]
-        public int SlowFactor
-        {
-            get => _ema.Period;
-            set
-            {
-                if (value <= 0)
-                    return;
+		private readonly EMA _emaAtrRsi = new EMA();
+		private readonly EMA _emaWilders = new EMA();
 
-                _ema.Period = value;
-                RecalculateValues();
-            }
-        }
+		private readonly RSI _rsi = new RSI();
+		private readonly EMA _rsiEma = new EMA();
+		private readonly ValueDataSeries _rsiMa = new ValueDataSeries("RsiMa");
+		private readonly ValueDataSeries _trLevelSlow = new ValueDataSeries("LevelSlow");
+		private readonly int _lastBar = -1;
 
-        public QQE()
-        {
-	        Panel = IndicatorDataProvider.NewPanel;
+		private bool _lastBarCounted;
 
-            _ema.Period = 5;
-            _rsi.Period = 14;
-            _emaWilders.Period = _emaAtrRsi.Period = _rsi.Period * 2 - 1;
+		#endregion
 
-            _trLevelSlow.LineDashStyle = LineDashStyle.Dash;
-            DataSeries[0] = _trLevelSlow;
-            DataSeries.Add(_rsiMa);
-        }
+		#region Properties
 
-        protected override void OnCalculate(int bar, decimal value)
-        {
-            if (_emaWilders.Period < SlowFactor && bar < SlowFactor
-            || _emaWilders.Period >= SlowFactor && bar < _emaWilders.Period)
-                return;
+		[Display(ResourceType = typeof(Resources), Name = "UseAlerts", GroupName = "Common")]
+		public bool UseAlerts { get; set; }
 
-            var candle = GetCandle(bar);
+		[Display(ResourceType = typeof(Resources), Name = "AlertFile", GroupName = "Common")]
+		public string AlertFile { get; set; }
 
-            var rsiValue = _rsi.Calculate(bar, candle.Close);
+		[Display(ResourceType = typeof(Resources), Name = "RSI", GroupName = "Common")]
+		public int RsiPeriod
+		{
+			get => _rsi.Period;
+			set
+			{
+				if (value <= 0)
+					return;
 
-            var emaRsiValue = _ema.Calculate(bar, rsiValue);
-            _rsiMa[bar] = Math.Abs(_ema[bar - 1] - emaRsiValue);
+				_rsi.Period = value;
+				_emaWilders.Period = value * 2 - 1;
+				_emaAtrRsi.Period = value * 2 - 1;
+				RecalculateValues();
+			}
+		}
 
+		[Display(ResourceType = typeof(Resources), Name = "SlowFactor", GroupName = "Common")]
+		public int SlowFactor
+		{
+			get => _rsiEma.Period;
+			set
+			{
+				if (value <= 0)
+					return;
 
-            var maAtrRsi = _emaWilders.Calculate(bar, _rsiMa[bar]);
+				_rsiEma.Period = value;
+				RecalculateValues();
+			}
+		}
 
-            var tr = _trLevelSlow[bar - 1];
+		#endregion
 
-            var rsi1 = _rsi[bar - 1];
+		#region ctor
 
-            var dar = _emaAtrRsi.Calculate(bar - 1, maAtrRsi) * 4.236m;
+		public QQE()
+			: base(true)
+		{
+			Panel = IndicatorDataProvider.NewPanel;
+			AlertFile = "alert1";
 
-            var dv = tr;
+			_rsiEma.Period = 5;
+			_rsi.Period = 14;
+			_emaWilders.Period = _emaAtrRsi.Period = _rsi.Period * 2 - 1;
 
-            if (rsi1 < tr)
-            {
-                tr = rsi1 + dar;
+			_rsiMa.Width = 2;
+			_trLevelSlow.LineDashStyle = LineDashStyle.Dash;
+			_trLevelSlow.Color = Colors.DodgerBlue;
 
-                if (rsi1 < dv && tr > dv)
-	                tr = dv;
-            }
-            else if (rsi1 > tr)
-            {
-	            tr = rsi1 - dar;
+			_rsiMa.Color = Colors.DarkBlue;
+			DataSeries[0] = _trLevelSlow;
+			DataSeries.Add(_rsiMa);
 
-	            if (rsi1 > dv && tr < dv)
-		            tr = dv;
-            }
+			LineSeries.Add(new LineSeries("TargetLevel")
+			{
+				Value = 50,
+				Color = Colors.Aqua
+			});
+		}
 
-            _trLevelSlow[bar - 1] = tr;
+		#endregion
 
-        }
+		#region Protected methods
 
-    }
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			if (bar == 0)
+			{
+				_lastBarCounted = false;
+				return;
+			}
+
+			var candle = GetCandle(bar);
+
+			var rsi = _rsi.Calculate(bar, candle.Close);
+			var rsiMa = _rsiEma.Calculate(bar, rsi);
+			var atrRsi = Math.Abs(_rsiEma[bar - 1] - rsiMa);
+			var maAtrRsi = _emaWilders.Calculate(bar, atrRsi);
+
+			if (_emaWilders.Period < SlowFactor && bar < SlowFactor
+				|| _emaWilders.Period >= SlowFactor && bar < _emaWilders.Period)
+				return;
+
+			_trLevelSlow[bar] = _trLevelSlow[bar - 1];
+			_rsiMa[bar] = rsiMa;
+
+			var dar = _emaAtrRsi.Calculate(bar, maAtrRsi) * QQEmultiplier;
+
+			var dv = _trLevelSlow[bar];
+
+			if (rsiMa < _trLevelSlow[bar])
+			{
+				_trLevelSlow[bar] = rsiMa + dar;
+
+				if (_rsiEma[bar - 1] < dv && _trLevelSlow[bar] > dv)
+					_trLevelSlow[bar] = dv;
+			}
+			else if (rsiMa > _trLevelSlow[bar])
+			{
+				_trLevelSlow[bar] = rsiMa - dar;
+
+				if (_rsiEma[bar - 1] > dv && _trLevelSlow[bar] < dv)
+					_trLevelSlow[bar] = dv;
+			}
+
+			if (_lastBar != bar)
+			{
+				if (_lastBarCounted && UseAlerts)
+				{
+					if (_rsiMa[bar - 1] < LineSeries[0].Value && _rsiMa[bar - 2] > LineSeries[0].Value
+						||
+						_rsiMa[bar - 1] > LineSeries[0].Value && _rsiMa[bar - 2] < LineSeries[0].Value
+					)
+						AddAlert(AlertFile, "Target level is achieved");
+				}
+
+				bar = _lastBar;
+			}
+			else
+			{
+				if (!_lastBarCounted)
+					_lastBarCounted = true;
+			}
+		}
+
+		#endregion
+	}
 }
