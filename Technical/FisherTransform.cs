fb8e7b1f40a5d46448598c6a254be5584fcb8404
Rinas Andrey <rinasandrey@gmail.com>
2020-10-27 12:46:55 +0700

    FisherTransform. Changed new bar detection logic, codestyle fix

--- Technical/FisherTransform.cs
+++ Technical/FisherTransform.cs
@@ -1,124 +1,106 @@
 ﻿namespace ATAS.Indicators.Technical
 {
-    using System;
-    using System.ComponentModel;
-    using System.ComponentModel.DataAnnotations;
-    using System.Windows.Media;
+	using System;
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+	using System.Windows.Media;
 
-    using ATAS.Indicators.Technical.Properties;
+	using ATAS.Indicators.Technical.Properties;
 
-    [DisplayName("Fisher Transform")]
-    public class FisherTransform : Indicator
-    {
-        #region Fields
+	[DisplayName("Fisher Transform")]
+	public class FisherTransform : Indicator
+	{
+		#region Fields
 
-        private readonly ValueDataSeries _fisher = new ValueDataSeries("Fisher");
+		private readonly ValueDataSeries _fisher = new ValueDataSeries("Fisher");
 
-        private readonly Highest _highest = new Highest();
-        private readonly Lowest _lowest = new Lowest();
+		private readonly Highest _highest = new Highest();
+		private readonly Lowest _lowest = new Lowest();
+		private readonly ValueDataSeries _triggers = new ValueDataSeries("Triggers");
 
-        private readonly ValueDataSeries _values = new ValueDataSeries("Values");
-        private readonly ValueDataSeries _triggers = new ValueDataSeries("Triggers");
-        private decimal _lastbar;
-        private decimal _lastFisher;
-        private decimal _lastValue;
-        private bool _lastBarCalculated;
+		private readonly ValueDataSeries _values = new ValueDataSeries("Values");
+		private readonly decimal _lastbar;
+		private decimal _lastFisher;
+		private decimal _lastValue;
 
-        #endregion
+		#endregion
 
-        #region Properties
+		#region Properties
 
-        [Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Common")]
-        public int Period
-        {
-            get => _highest.Period;
-            set
-            {
-                if (value <= 0)
-                    return;
+		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Common")]
+		public int Period
+		{
+			get => _highest.Period;
+			set
+			{
+				if (value <= 0)
+					return;
 
-                _highest.Period = value;
-                _lowest.Period = value;
-                RecalculateValues();
-            }
-        }
+				_highest.Period = value;
+				_lowest.Period = value;
+				RecalculateValues();
+			}
+		}
 
-        #endregion
+		#endregion
 
-        #region ctor
+		#region ctor
 
-        public FisherTransform()
-            : base(true)
-        {
-            Panel = IndicatorDataProvider.NewPanel;
+		public FisherTransform()
+			: base(true)
+		{
+			Panel = IndicatorDataProvider.NewPanel;
 
-            _lastbar = -1;
-            _highest.Period = _lowest.Period = 10;
+			_lastbar = -1;
+			_highest.Period = _lowest.Period = 10;
 
-            _triggers.Color = Colors.Red;
-            _fisher.Color = Colors.DodgerBlue;
+			_triggers.Color = Colors.Red;
+			_fisher.Color = Colors.DodgerBlue;
 
-            DataSeries[0] = _triggers;
-            DataSeries.Add(_fisher);
-        }
+			DataSeries[0] = _triggers;
+			DataSeries.Add(_fisher);
+		}
 
-        #endregion
+		#endregion
 
-        #region Protected methods
+		#region Protected methods
 
-        protected override void OnCalculate(int bar, decimal value)
-        {
-            if (bar == 0)
-            {
-                _lastBarCalculated = false;
-            }
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			if (bar > Period)
+			{
+				if (bar != _lastbar && bar > 0)
+				{
+					_lastValue = _values[bar - 1];
+					_lastFisher = _fisher[bar - 1];
+				}
 
-            if (bar > Period)
-            {
-                if (bar == _lastbar)
-                {
-                    _lastValue = _values[bar - 1];
-                    _lastFisher = _fisher[bar - 1];
-                }
+				var candle = GetCandle(bar);
 
-                var candle = GetCandle(bar);
+				var smax = _highest.Calculate(bar, candle.High);
+				var smin = _lowest.Calculate(bar, candle.Close);
 
-                var smax = _highest.Calculate(bar, candle.High);
-                var smin = _lowest.Calculate(bar, candle.Close);
+				if (smax == smin)
+					smax += ChartInfo.PriceChartContainer.Step;
 
-                if (smax == smin)
-                    smax += ChartInfo.PriceChartContainer.Step;
+				var wpr = (candle.Close - smin) / (smax - smin);
 
-                var wpr = (candle.Close - smin) / (smax - smin);
+				var valueSeries = 0.66m * (wpr - 0.5m) + 0.67m * _lastValue;
 
-                var valueSeries = 0.66m * (wpr - 0.5m) + 0.67m * _lastValue;
+				if (valueSeries >= 1 || valueSeries <= -1)
+					valueSeries = Math.Sign(valueSeries) * 0.999m;
 
-                if (valueSeries >= 1 || valueSeries <= -1)
-                    valueSeries = Math.Sign(valueSeries) * 0.999m;
+				var fisherSeries = 0.5 * Math.Log((1.0 + Convert.ToDouble(valueSeries)) / (1.0 - Convert.ToDouble(valueSeries))) +
+					0.5 * Convert.ToDouble(_lastFisher);
 
-                _values[bar] = valueSeries;
+				fisherSeries = Math.Round(fisherSeries, 5);
 
+				_values[bar] = valueSeries;
+				_fisher[bar] = Convert.ToDecimal(fisherSeries);
+				_triggers[bar] = _lastFisher;
+			}
+		}
 
-                var fisherSeries = 0.5 * Math.Log((1.0 + Convert.ToDouble(valueSeries)) / (1.0 - Convert.ToDouble(valueSeries))) +
-                    0.5 * Convert.ToDouble(_lastFisher);
-                fisherSeries = Math.Round(fisherSeries, 5);
-
-                _fisher[bar] = Convert.ToDecimal(fisherSeries);
-                _triggers[bar] = _lastFisher;
-
-                if (bar != _lastbar)
-                {
-                    _lastbar = bar;
-
-
-                    _lastValue = valueSeries;
-                    _lastFisher = _fisher[bar];
-
-                }
-
-            }
-        }
-
-        #endregion
-    }
+		#endregion
+	}
 }
