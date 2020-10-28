4da4dcd17b8e5370ca6bbd378c57c33d3363c115
olinyavod_zh <olinyavod@gmail.com>
2020-10-28 11:26:16 +1000

    Indicators. Remove DevExpress reference (https://trello.com/c/BI0f3bD7)

--- Technical/Watermark.cs
+++ Technical/Watermark.cs
@@ -4,6 +4,7 @@
 	using System.ComponentModel.DataAnnotations;
 	using System.Drawing;
 
+	using ATAS.Indicators.Editors;
 	using ATAS.Indicators.Technical.Properties;
 
 	using OFT.Rendering.Context;
