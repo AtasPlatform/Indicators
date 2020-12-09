ef5cd493b15ed264e368aa8133ae0967f9c0a96f
Stig <Stan422@gmail.com>
2020-12-09 16:49:38 +0700

    ATR Normalized added

---
+++ Technical/ATRN.cs
@@ -0,0 +1,60 @@
+﻿namespace ATAS.Indicators.Technical
+{
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+
+	using ATAS.Indicators.Technical.Properties;
+
+	[DisplayName("ATR Normalized")]
+	public class ATRN : Indicator
+	{
+		#region Fields
+
+		private readonly ATR _atr = new ATR();
+
+		private readonly ValueDataSeries _renderSeries = new ValueDataSeries(Resources.Visualization);
+
+		#endregion
+
+		#region Properties
+
+		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Settings", Order = 100)]
+		public int CalcMode
+		{
+			get => _atr.Period;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_atr.Period = value;
+				RecalculateValues();
+			}
+		}
+
+		#endregion
+
+		#region ctor
+
+		public ATRN()
+			: base(true)
+		{
+			Panel = IndicatorDataProvider.NewPanel;
+
+			_atr.Period = 10;
+			Add(_atr);
+			DataSeries[0] = _renderSeries;
+		}
+
+		#endregion
+
+		#region Protected methods
+
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			_renderSeries[bar] = 100m * ((ValueDataSeries)_atr.DataSeries[0])[bar] / GetCandle(bar).Close;
+		}
+
+		#endregion
+	}
+}
