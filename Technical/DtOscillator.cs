025de69dffb6547dde046c00b851566d988cf055
Stig <Stan422@gmail.com>
2020-12-10 17:24:59 +0700

    DtOscillator added

---
+++ Technical/DtOscillator.cs
@@ -0,0 +1,114 @@
+﻿namespace ATAS.Indicators.Technical
+{
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+	using System.Windows.Media;
+
+	using ATAS.Indicators.Technical.Properties;
+
+	[DisplayName("DT Oscillator")]
+	public class DtOscillator : Indicator
+	{
+		#region Fields
+
+		private readonly ValueDataSeries _sdSeries = new ValueDataSeries(Resources.SMMA);
+
+		private readonly ValueDataSeries _skSeries = new ValueDataSeries(Resources.SMA);
+		private readonly SMA _smaSd = new SMA();
+		private readonly SMA _smaSk = new SMA();
+		private readonly StochasticRsi _stRsi = new StochasticRsi();
+
+		#endregion
+
+		#region Properties
+
+		[Display(ResourceType = typeof(Resources), Name = "RSI", GroupName = "Stochastic", Order = 100)]
+		public int RsiPeriod
+		{
+			get => _stRsi.RsiPeriod;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_stRsi.RsiPeriod = value;
+				RecalculateValues();
+			}
+		}
+
+		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Stochastic", Order = 110)]
+		public int Period
+		{
+			get => _stRsi.Period;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_stRsi.Period = value;
+				RecalculateValues();
+			}
+		}
+
+		[Display(ResourceType = typeof(Resources), Name = "SMAPeriod1", GroupName = "Smooth", Order = 200)]
+		public int SMAPeriod1
+		{
+			get => _smaSk.Period;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_smaSk.Period = value;
+				RecalculateValues();
+			}
+		}
+
+		[Display(ResourceType = typeof(Resources), Name = "SMAPeriod2", GroupName = "Smooth", Order = 210)]
+		public int SMAPeriod2
+		{
+			get => _smaSd.Period;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_smaSd.Period = value;
+				RecalculateValues();
+			}
+		}
+
+		#endregion
+
+		#region ctor
+
+		public DtOscillator()
+		{
+			Panel = IndicatorDataProvider.NewPanel;
+			_stRsi.RsiPeriod = 8;
+			_stRsi.Period = 5;
+
+			_smaSk.Period = 3;
+			_smaSd.Period = 3;
+
+			_skSeries.Color = Colors.Red;
+			_sdSeries.Color = Colors.Blue;
+
+			DataSeries[0] = _skSeries;
+			DataSeries.Add(_sdSeries);
+		}
+
+		#endregion
+
+		#region Protected methods
+
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			var stochRsi = _stRsi.Calculate(bar, value);
+			_skSeries[bar] = _smaSk.Calculate(bar, 100 * stochRsi);
+			_sdSeries[bar] = _smaSd.Calculate(bar, _skSeries[bar]);
+		}
+
+		#endregion
+	}
+}
