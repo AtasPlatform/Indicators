00b6375d870a08c8b667b49f6619285154a03b9e
Stig <Stan422@gmail.com>
2020-12-11 12:27:05 +0700

    DirectionalMovementOscillator added

---
+++ Technical/DmOscillator.cs
@@ -0,0 +1,59 @@
+﻿namespace ATAS.Indicators.Technical
+{
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+
+	using ATAS.Indicators.Technical.Properties;
+
+	[DisplayName("Directional Movement Oscillator")]
+	public class DmOscillator : Indicator
+	{
+		#region Fields
+
+		private readonly DmIndex _dm = new DmIndex();
+
+		private readonly ValueDataSeries _renderSeries = new ValueDataSeries(Resources.Visualization);
+
+		#endregion
+
+		#region Properties
+
+		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Settings", Order = 110)]
+		public int Period
+		{
+			get => _dm.Period;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_dm.Period = value;
+				RecalculateValues();
+			}
+		}
+
+		#endregion
+
+		#region ctor
+
+		public DmOscillator()
+			: base(true)
+		{
+			Panel = IndicatorDataProvider.NewPanel;
+			_dm.Period = 14;
+			Add(_dm);
+			DataSeries[0] = _renderSeries;
+		}
+
+		#endregion
+
+		#region Protected methods
+
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			_renderSeries[bar] = ((ValueDataSeries)_dm.DataSeries[0])[bar] - ((ValueDataSeries)_dm.DataSeries[1])[bar];
+		}
+
+		#endregion
+	}
+}
