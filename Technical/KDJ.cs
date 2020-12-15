f1ba8a6e4fb0b3e113ede2663068c48ea11f25f7
Stig <Stan422@gmail.com>
2020-12-15 16:12:08 +0700

    KDJ added

---
+++ Technical/KDJ.cs
@@ -0,0 +1,89 @@
+﻿namespace ATAS.Indicators.Technical
+{
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+
+	using ATAS.Indicators.Technical.Properties;
+
+	[DisplayName("KDJ")]
+	public class KDJ : Indicator
+	{
+		#region Fields
+
+		private readonly ValueDataSeries _renderSeries = new ValueDataSeries(Resources.Visualization);
+		private readonly KdSlow _kdSlow = new KdSlow();
+
+		#endregion
+
+		#region Properties
+
+		[Display(ResourceType = typeof(Resources), Name = "PeriodK", GroupName = "ShortPeriod", Order = 100)]
+		public int PeriodK
+		{
+			get => _kdSlow.PeriodK;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_kdSlow.PeriodK = value;
+				RecalculateValues();
+			}
+		}
+
+		[Display(ResourceType = typeof(Resources), Name = "PeriodD", GroupName = "ShortPeriod", Order = 110)]
+		public int PeriodD
+		{
+			get => _kdSlow.PeriodD;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_kdSlow.PeriodD = value;
+				RecalculateValues();
+			}
+		}
+
+		[Display(ResourceType = typeof(Resources), Name = "PeriodD", GroupName = "LongPeriod", Order = 120)]
+		public int SlowPeriodD
+		{
+			get => _kdSlow.SlowPeriodD;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_kdSlow.SlowPeriodD = value;
+				RecalculateValues();
+			}
+		}
+
+		#endregion
+
+		#region ctor
+
+		public KDJ()
+			: base(true)
+		{
+			Panel = IndicatorDataProvider.NewPanel;
+
+			_kdSlow.SlowPeriodD = _kdSlow.PeriodK = _kdSlow.PeriodD = 10;
+
+			Add(_kdSlow);
+			DataSeries[0] = _renderSeries;
+		}
+
+		#endregion
+
+		#region Protected methods
+
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			_renderSeries[bar] = 3 * ((ValueDataSeries)_kdSlow.DataSeries[0])[bar] -
+				2 * ((ValueDataSeries)_kdSlow.DataSeries[1])[bar];
+		}
+
+		#endregion
+	}
+}
