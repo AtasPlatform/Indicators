33f3ad53fe9cc1535653a9ed32fe79c1686d3a83
Stig <Stan422@gmail.com>
2020-12-17 16:36:03 +0700

    SZMA added

---
+++ Technical/SZMA.cs
@@ -0,0 +1,71 @@
+﻿namespace ATAS.Indicators.Technical
+{
+	using System;
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+
+	using ATAS.Indicators.Technical.Properties;
+
+	[DisplayName("Simple Moving Average - Skip Zeros")]
+	public class SZMA : Indicator
+	{
+		#region Fields
+
+		private readonly ValueDataSeries _renderSeries = new ValueDataSeries(Resources.Visualization);
+		private int _period;
+
+		#endregion
+
+		#region Properties
+
+		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Settings", Order = 100)]
+		public int Period
+		{
+			get => _period;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_period = value;
+				RecalculateValues();
+			}
+		}
+
+		#endregion
+
+		#region ctor
+
+		public SZMA()
+		{
+			_period = 10;
+			DataSeries[0] = _renderSeries;
+		}
+
+		#endregion
+
+		#region Protected methods
+
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			var sum = 0m;
+			var nonZeroValues = 0;
+
+			for (var i = Math.Max(0, bar - _period); i <= bar; i++)
+			{
+				if ((decimal)SourceDataSeries[i] == 0)
+					continue;
+
+				sum += (decimal)SourceDataSeries[i];
+				nonZeroValues++;
+			}
+
+			_renderSeries[bar] = 0;
+
+			if (nonZeroValues != 0)
+				_renderSeries[bar] = sum / nonZeroValues;
+		}
+
+		#endregion
+	}
+}
