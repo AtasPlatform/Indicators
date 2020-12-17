129b047af8f437081903769f58df7a229a56d108
Stig <Stan422@gmail.com>
2020-12-17 14:14:57 +0700

    DEMA added

---
+++ Technical/DEMA.cs
@@ -0,0 +1,59 @@
+﻿namespace ATAS.Indicators.Technical
+{
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+
+	using ATAS.Indicators.Technical.Properties;
+
+	[DisplayName("Double Exponential Moving Average")]
+	public class DEMA : Indicator
+	{
+		#region Fields
+
+		private readonly EMA _emaFirst = new EMA();
+		private readonly EMA _emaSecond = new EMA();
+
+		private readonly ValueDataSeries _renderSeries = new ValueDataSeries(Resources.Visualization);
+
+		#endregion
+
+		#region Properties
+
+		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Settings", Order = 100)]
+		public int Period
+		{
+			get => _emaFirst.Period;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_emaFirst.Period = _emaSecond.Period = value;
+				RecalculateValues();
+			}
+		}
+
+		#endregion
+
+		#region ctor
+
+		public DEMA()
+		{
+			_emaFirst.Period = _emaSecond.Period = 10;
+			DataSeries[0] = _renderSeries;
+		}
+
+		#endregion
+
+		#region Protected methods
+
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			_emaFirst.Calculate(bar, value);
+			_emaSecond.Calculate(bar, _emaFirst[bar]);
+			_renderSeries[bar] = 2 * _emaFirst[bar] - _emaSecond[bar];
+		}
+
+		#endregion
+	}
+}
