d5ea6e65d1bfd631d2d8b1174212bd8fdd7f4c73
Stig <Stan422@gmail.com>
2020-12-24 17:27:52 +0700

    Momentum show MA option added

--- Technical/Momentum.cs
+++ Technical/Momentum.cs
@@ -3,6 +3,7 @@ namespace ATAS.Indicators.Technical
 	using System;
 	using System.ComponentModel;
 	using System.ComponentModel.DataAnnotations;
+	using System.Windows.Media;
 
 	using ATAS.Indicators.Technical.Properties;
 
@@ -18,16 +19,14 @@ namespace ATAS.Indicators.Technical
 		#region Fields
 
 		private int _period;
+		private readonly SMA _sma = new SMA();
+		private readonly ValueDataSeries _smaSeries = new ValueDataSeries(Resources.SMA);
 
 		#endregion
 
 		#region Properties
 
-		[Parameter]
-		[Display(ResourceType = typeof(Resources),
-			Name = "Period",
-			GroupName = "Common",
-			Order = 20)]
+		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Common", Order = 20)]
 		public int Period
 		{
 			get => _period;
@@ -41,6 +40,27 @@ namespace ATAS.Indicators.Technical
 			}
 		}
 
+		[Display(ResourceType = typeof(Resources), Name = "ShowSMA", GroupName = "SMA", Order = 200)]
+		public bool ShowSma
+		{
+			get => _smaSeries.VisualType == VisualMode.Line;
+			set => _smaSeries.VisualType = value ? VisualMode.Line : VisualMode.Hide;
+		}
+
+		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "SMA", Order = 210)]
+		public int SmaPeriod
+		{
+			get => _sma.Period;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_sma.Period = value;
+				RecalculateValues();
+			}
+		}
+
 		#endregion
 
 		#region ctor
@@ -49,6 +69,8 @@ namespace ATAS.Indicators.Technical
 		{
 			Panel = IndicatorDataProvider.NewPanel;
 			Period = 10;
+			_smaSeries.Color = Colors.Blue;
+			DataSeries.Add(_smaSeries);
 		}
 
 		#endregion
@@ -59,6 +81,7 @@ namespace ATAS.Indicators.Technical
 		{
 			var start = Math.Max(0, bar - Period + 1);
 			this[bar] = value - (decimal)SourceDataSeries[start];
+			_smaSeries[bar] = _sma.Calculate(bar, this[bar]);
 		}
 
 		#endregion
