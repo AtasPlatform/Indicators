60dce0ed7f4ebc52f643ab8c3f9bec08ef30ce20
Stig <Stan422@gmail.com>
2020-12-28 18:42:33 +0700

    VolatilityChaikins added

---
+++ Technical/VolatilityChaikins.cs
@@ -0,0 +1,68 @@
+﻿namespace ATAS.Indicators.Technical
+{
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+
+	using ATAS.Indicators.Technical.Properties;
+
+	[DisplayName("Volatility - Chaikins")]
+	public class VolatilityChaikins : Indicator
+	{
+		#region Fields
+
+		private readonly EMA _ema = new EMA();
+		private readonly ValueDataSeries _renderSeries = new ValueDataSeries(Resources.Visualization);
+
+		#endregion
+
+		#region Properties
+
+		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Settings", Order = 100)]
+		public int Period
+		{
+			get => _ema.Period;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_ema.Period = value;
+				RecalculateValues();
+			}
+		}
+
+		#endregion
+
+		#region ctor
+
+		public VolatilityChaikins()
+			: base(true)
+		{
+			Panel = IndicatorDataProvider.NewPanel;
+			_ema.Period = 10;
+
+			DataSeries[0] = _renderSeries;
+		}
+
+		#endregion
+
+		#region Protected methods
+
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			if (bar == 0)
+				_renderSeries.Clear();
+
+			var candle = GetCandle(bar);
+			_ema.Calculate(bar, candle.High - candle.Low);
+
+			if (bar < Period)
+				return;
+
+			if (_ema[bar] != 0)
+				_renderSeries[bar] = 100 * (_ema[bar] - _ema[bar - Period]) / _ema[bar - Period];
+		}
+
+		#endregion
+	}
+}
