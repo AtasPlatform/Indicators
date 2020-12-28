375e57cffc5ef968fec553096a8694155c6a7971
Stig <Stan422@gmail.com>
2020-12-28 18:27:56 +0700

    Volatility History added

---
+++ Technical/VolatilityHist.cs
@@ -0,0 +1,76 @@
+﻿namespace ATAS.Indicators.Technical
+{
+	using System;
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+
+	using ATAS.Indicators.Technical.Properties;
+
+	[DisplayName("Volatility - Historical")]
+	public class VolatilityHist : Indicator
+	{
+		#region Fields
+
+		private readonly ValueDataSeries _diffSquareSeries = new ValueDataSeries("Diff");
+		private readonly ValueDataSeries _renderSeries = new ValueDataSeries(Resources.Visualization);
+
+		private readonly SMA _sma = new SMA();
+
+		#endregion
+
+		#region Properties
+
+		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Settings", Order = 100)]
+		public int Period
+		{
+			get => _sma.Period;
+			set
+			{
+				if (value <= 1)
+					return;
+
+				_sma.Period = value;
+				RecalculateValues();
+			}
+		}
+
+		#endregion
+
+		#region ctor
+
+		public VolatilityHist()
+		{
+			Panel = IndicatorDataProvider.NewPanel;
+			_sma.Period = 10;
+
+			DataSeries[0] = _renderSeries;
+		}
+
+		#endregion
+
+		#region Protected methods
+
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			if (bar == 0)
+			{
+				_renderSeries.Clear();
+				_sma.Calculate(bar, 0);
+				return;
+			}
+
+			var lr = (decimal)Math.Log((double)(value / (decimal)SourceDataSeries[bar - 1]));
+			_sma.Calculate(bar, lr);
+
+			var diff = lr - _sma[bar];
+			_diffSquareSeries[bar] = diff * diff;
+
+			if (bar < Period)
+				return;
+
+			_renderSeries[bar] = 100 * (decimal)(Math.Sqrt(CurrentBar) * Math.Sqrt((double)_diffSquareSeries.CalcSum(Period, bar) / (Period - 1)));
+		}
+
+		#endregion
+	}
+}
