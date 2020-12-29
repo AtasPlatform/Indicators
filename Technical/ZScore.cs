f91f479df5fa27e51926799b54d7c8b6a9aab430
Stig <Stan422@gmail.com>
2020-12-29 15:30:46 +0700

    ZScore added

---
+++ Technical/ZScore.cs
@@ -0,0 +1,80 @@
+﻿namespace ATAS.Indicators.Technical
+{
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+
+	using ATAS.Indicators.Technical.Properties;
+
+	[DisplayName("Z-Score")]
+	public class ZScore : Indicator
+	{
+		#region Fields
+
+		private readonly ValueDataSeries _renderSeries = new ValueDataSeries(Resources.Visualization);
+
+		private readonly SMA _sma = new SMA();
+		private readonly StdDev _stdDev = new StdDev();
+
+		#endregion
+
+		#region Properties
+
+		[Display(ResourceType = typeof(Resources), Name = "SMA", GroupName = "Period", Order = 100)]
+		public int SmaPeriod
+		{
+			get => _sma.Period;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_sma.Period = value;
+				RecalculateValues();
+			}
+		}
+
+		[Display(ResourceType = typeof(Resources), Name = "StdDev", GroupName = "Period", Order = 110)]
+		public int StdPeriod
+		{
+			get => _stdDev.Period;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_stdDev.Period = value;
+				RecalculateValues();
+			}
+		}
+
+		#endregion
+
+		#region ctor
+
+		public ZScore()
+		{
+			Panel = IndicatorDataProvider.NewPanel;
+
+			_stdDev.Period = 10;
+			_sma.Period = 10;
+			DataSeries[0] = _renderSeries;
+		}
+
+		#endregion
+
+		#region Protected methods
+
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			_sma.Calculate(bar, value);
+			_stdDev.Calculate(bar, value);
+
+			if (_stdDev[bar] != 0)
+				_renderSeries[bar] = (value - _sma[bar]) / _stdDev[bar];
+			else
+				_renderSeries[bar] = 0;
+		}
+
+		#endregion
+	}
+}
