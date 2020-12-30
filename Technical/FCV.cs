55dacef735a287159b9734ba8ccd8620b035da13
Stig <Stan422@gmail.com>
2020-12-30 14:42:21 +0700

    FCV added

---
+++ Technical/FCV.cs
@@ -0,0 +1,72 @@
+﻿namespace ATAS.Indicators.Technical
+{
+	using System;
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+
+	using ATAS.Indicators.Technical.Properties;
+
+	[DisplayName("Full Contract Value")]
+	public class FCV : Indicator
+	{
+		#region Fields
+
+		private readonly ValueDataSeries _renderSeries = new ValueDataSeries(Resources.Visualization);
+		private bool _customScale;
+		private decimal _multiplier;
+
+		#endregion
+
+		#region Properties
+
+		[Display(ResourceType = typeof(Resources), Name = "Enabled", GroupName = "Multiplier", Order = 100)]
+		public bool CustomScale
+		{
+			get => _customScale;
+			set
+			{
+				_customScale = value;
+				RecalculateValues();
+			}
+		}
+
+		[Display(ResourceType = typeof(Resources), Name = "Value", GroupName = "Multiplier", Order = 110)]
+		public decimal Multiplier
+		{
+			get => _multiplier;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_multiplier = value;
+				RecalculateValues();
+			}
+		}
+
+		#endregion
+
+		#region ctor
+
+		public FCV()
+		{
+			Panel = IndicatorDataProvider.NewPanel;
+
+			DataSeries[0] = _renderSeries;
+		}
+
+		#endregion
+
+		#region Protected methods
+
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			if (bar == 0 && !_customScale)
+				_multiplier = InstrumentInfo.TickSize;
+
+			_renderSeries[bar] = value * Math.Max(InstrumentInfo.TickSize, _multiplier) / InstrumentInfo.TickSize;
+		}
+
+		#endregion
+	}
+}
