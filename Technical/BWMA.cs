43881a6e0ebf323b6dc51ad7c7ec8ebebc8f6e4f
Stig <Stan422@gmail.com>
2021-04-02 10:56:04 +0700

    Bill Williams Moving Average added

---
+++ Technical/BWMA.cs
@@ -0,0 +1,64 @@
+﻿namespace ATAS.Indicators.Technical
+{
+	using System.ComponentModel;
+	using System.ComponentModel.DataAnnotations;
+	using System.Windows.Media;
+
+	using ATAS.Indicators.Technical.Properties;
+
+	[DisplayName("Bill Williams Moving Average")]
+	public class BWMA : Indicator
+	{
+		#region Fields
+
+		private readonly ValueDataSeries _renderSeries = new(Resources.Visualization);
+		private int _period;
+
+		#endregion
+
+		#region Properties
+
+		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Settings", Order = 100)]
+		public int Period
+		{
+			get => _period;
+			set
+			{
+				if (value <= 0)
+					return;
+
+				_period = value;
+				RecalculateValues();
+			}
+		}
+
+		#endregion
+
+		#region ctor
+
+		public BWMA()
+		{
+			_period = 10;
+			_renderSeries.Color = Colors.Blue;
+			DataSeries[0] = _renderSeries;
+		}
+
+		#endregion
+
+		#region Protected methods
+
+		protected override void OnCalculate(int bar, decimal value)
+		{
+			if (bar == 0)
+			{
+				_renderSeries.Clear();
+				_renderSeries[bar] = value;
+				return;
+			}
+
+			_renderSeries[bar] = (1m - 1m / _period) * _renderSeries[bar - 1] + 1m / _period * value;
+		}
+
+		#endregion
+	}
+}
