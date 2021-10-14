920b44ec1dbcde531c6a7bc96ab8b99865e8e888
Stig <Stan422@gmail.com>
2021-10-14 13:07:46 +0700

    Stochastic - Percentile visual issues fix
    https://trello.com/c/ObOqVuWi

--- Technical/StohasticPercentile.cs
+++ Technical/StohasticPercentile.cs
@@ -1,100 +1,102 @@
-﻿namespace ATAS.Indicators.Technical
+﻿namespace ATAS.Indicators.Technical;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+using ATAS.Indicators.Technical.Properties;
+
+using OFT.Attributes;
+
+[DisplayName("Stochastic - Percentile")]
+[HelpLink("https://support.atas.net/ru/knowledge-bases/2/articles/45493-stochastic-percentile")]
+public class StohasticPercentile : Indicator
 {
-	using System.Collections.Generic;
-	using System.ComponentModel;
-	using System.ComponentModel.DataAnnotations;
-	using System.Linq;
+	#region Fields
 
-	using ATAS.Indicators.Technical.Properties;
+	private readonly ValueDataSeries _renderSeries = new(Resources.Visualization);
 
-	using OFT.Attributes;
+	private readonly SMA _sma = new();
+	private readonly List<decimal> _values = new();
+	private int _lastBar;
+	private int _period;
 
-	[DisplayName("Stochastic - Percentile")]
-	[HelpLink("https://support.atas.net/ru/knowledge-bases/2/articles/45493-stochastic-percentile")]
-	public class StohasticPercentile : Indicator
+	#endregion
+
+	#region Properties
+
+	[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Settings", Order = 100)]
+	public int Period
 	{
-		#region Fields
-
-		private readonly ValueDataSeries _renderSeries = new(Resources.Visualization);
-
-		private readonly SMA _sma = new();
-		private readonly List<decimal> _values = new();
-		private int _lastBar;
-		private int _period;
-
-		#endregion
-
-		#region Properties
-
-		[Display(ResourceType = typeof(Resources), Name = "Period", GroupName = "Settings", Order = 100)]
-		public int Period
+		get => _period;
+		set
 		{
-			get => _period;
-			set
-			{
-				if (value <= 1)
-					return;
+			if (value <= 1)
+				return;
 
-				_period = value;
-				RecalculateValues();
-			}
+			_period = value;
+			RecalculateValues();
 		}
-
-		[Display(ResourceType = typeof(Resources), Name = "SMA", GroupName = "Settings", Order = 110)]
-		public int SmaPeriod
-		{
-			get => _sma.Period;
-			set
-			{
-				if (value <= 0)
-					return;
-
-				_sma.Period = value;
-				RecalculateValues();
-			}
-		}
-
-		#endregion
-
-		#region ctor
-
-		public StohasticPercentile()
-		{
-			Panel = IndicatorDataProvider.NewPanel;
-			_sma.Period = _period = 10;
-			_lastBar = -1;
-			DataSeries[0] = _renderSeries;
-		}
-
-		#endregion
-
-		#region Protected methods
-
-		protected override void OnCalculate(int bar, decimal value)
-		{
-			if (bar == 0)
-			{
-				_values.Clear();
-				_renderSeries.Clear();
-			}
-
-			if (_values.Count > _period)
-				_values.RemoveAt(0);
-
-			if (bar == _lastBar)
-				_values.RemoveAt(_values.Count - 1);
-
-			_values.Add(value);
-
-			var rankedValues = _values.OrderBy(x => x).ToList();
-
-			var sp = 100m * rankedValues.IndexOf(value) / (_period - 1);
-
-			_renderSeries[bar] = _sma.Calculate(bar, sp);
-
-			_lastBar = bar;
-		}
-
-		#endregion
 	}
+
+	[Display(ResourceType = typeof(Resources), Name = "SMA", GroupName = "Settings", Order = 110)]
+	public int SmaPeriod
+	{
+		get => _sma.Period;
+		set
+		{
+			if (value <= 0)
+				return;
+
+			_sma.Period = value;
+			RecalculateValues();
+		}
+	}
+
+	#endregion
+
+	#region ctor
+
+	public StohasticPercentile()
+	{
+		Panel = IndicatorDataProvider.NewPanel;
+		_sma.Period = _period = 10;
+		_lastBar = -1;
+		DataSeries[0] = _renderSeries;
+	}
+
+	#endregion
+
+	#region Protected methods
+
+	protected override void OnCalculate(int bar, decimal value)
+	{
+		if (bar == 0)
+		{
+			_values.Clear();
+			_renderSeries.Clear();
+		}
+
+		if (_values.Count > _period)
+			_values.RemoveAt(0);
+
+		if (bar == _lastBar)
+			_values.RemoveAt(_values.Count - 1);
+
+		_values.Add(value);
+
+		var rankedValues = _values.OrderBy(x => x).ToList();
+
+		var sp = 100m * rankedValues.IndexOf(value) / (_period - 1);
+
+		_sma.Calculate(bar, sp);
+
+		_renderSeries[bar] = (decimal)Math.Max(0.000001, (double)_sma[bar]);
+
+		_lastBar = bar;
+	}
+
+	#endregion
 }
