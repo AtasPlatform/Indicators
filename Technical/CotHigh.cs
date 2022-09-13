c0fdc61df13787fe32425a74eedb94e4aea5f463
Stig <Stan422@gmail.com>
2022-09-13 14:27:33 +0700

    Cot High/Low low mode is added

--- Technical/CotHigh.cs
+++ Technical/CotHigh.cs
@@ -1,87 +1,122 @@
-﻿namespace ATAS.Indicators.Technical
+﻿namespace ATAS.Indicators.Technical;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Windows.Media;
+
+using ATAS.Indicators.Technical.Properties;
+
+[DisplayName("COT High/Low")]
+public class CotHigh : Indicator
 {
-	using System.ComponentModel;
-	using System.Windows.Media;
+	#region Nested types
 
-	using ATAS.Indicators.Technical.Properties;
-
-	using OFT.Attributes;
-
-	[DisplayName("COT High")]
-	public class CotHigh : Indicator
+	public enum CotMode
 	{
-		#region Fields
+		[Display(ResourceType = typeof(Resources), Name = "High")]
+		High,
 
-		private readonly ValueDataSeries _negSeries = new(Resources.Negative);
-		private readonly ValueDataSeries _posSeries = new(Resources.Positive);
-		private decimal _high;
-		private decimal _levelBar;
-
-		#endregion
-
-		#region ctor
-
-		public CotHigh()
-			: base(true)
-		{
-			Panel = IndicatorDataProvider.NewPanel;
-
-			_posSeries.VisualType = _negSeries.VisualType = VisualMode.Histogram;
-			_posSeries.Color = Colors.Green;
-			_negSeries.Color = Colors.Red;
-
-			DataSeries[0] = _posSeries;
-			DataSeries.Add(_negSeries);
-		}
-
-		#endregion
-
-		#region Protected methods
-
-		protected override void OnCalculate(int bar, decimal value)
-		{
-			if (bar == 0)
-			{
-				_high = 0;
-				_levelBar = 0;
-				DataSeries.ForEach(x => x.Clear());
-			}
-
-			var candle = GetCandle(bar);
-
-			if (candle.High >= _high)
-			{
-				_high = candle.High;
-				_levelBar = bar;
-				DrawValue(bar, candle.Delta);
-			}
-			else
-			{
-				var renderValue = LastValue(bar) + candle.Delta;
-				DrawValue(bar, renderValue);
-			}
-		}
-
-		#endregion
-
-		#region Private methods
-
-		private void DrawValue(int bar, decimal value)
-		{
-			if (value > 0)
-				_posSeries[bar] = value;
-			else
-				_negSeries[bar] = value;
-		}
-
-		private decimal LastValue(int bar)
-		{
-			return
-				_posSeries[bar - 1] != 0
-					? _posSeries[bar - 1]
-					: _negSeries[bar - 1];
-		}
-
-		#endregion
+		[Display(ResourceType = typeof(Resources), Name = "Low")]
+		Low
 	}
+
+	#endregion
+
+	#region Fields
+
+	private readonly ValueDataSeries _negSeries = new(Resources.Negative);
+	private readonly ValueDataSeries _posSeries = new(Resources.Positive);
+	private decimal _extValue;
+	private CotMode _mode = CotMode.High;
+
+	#endregion
+
+	#region Properties
+
+	[Display(ResourceType = typeof(Resources), Name = "CalculationMode", GroupName = "Settings", Order = 100)]
+	public CotMode Mode
+	{
+		get => _mode;
+		set
+		{
+			_mode = value;
+			RecalculateValues();
+		}
+	}
+
+	#endregion
+
+	#region ctor
+
+	public CotHigh()
+		: base(true)
+	{
+		Panel = IndicatorDataProvider.NewPanel;
+
+		_posSeries.VisualType = _negSeries.VisualType = VisualMode.Histogram;
+		_posSeries.Color = Colors.Green;
+		_negSeries.Color = Colors.Red;
+		_posSeries.ShowZeroValue = _negSeries.ShowZeroValue = false;
+
+		DataSeries[0] = _posSeries;
+		DataSeries.Add(_negSeries);
+	}
+
+	#endregion
+
+	#region Protected methods
+
+	protected override void OnCalculate(int bar, decimal value)
+	{
+		if (bar == 0)
+		{
+			_extValue = 0;
+			DataSeries.ForEach(x => x.Clear());
+		}
+
+		var candle = GetCandle(bar);
+
+		if ((candle.High >= _extValue && Mode is CotMode.High)
+		    ||
+		    (candle.Low >= _extValue && Mode is CotMode.High))
+		{
+			_extValue = Mode is CotMode.High
+				? candle.High
+				: candle.Low;
+			DrawValue(bar, candle.Delta);
+		}
+		else
+		{
+			var renderValue = LastValue(bar) + candle.Delta;
+			DrawValue(bar, renderValue);
+		}
+	}
+
+	#endregion
+
+	#region Private methods
+
+	private void DrawValue(int bar, decimal value)
+	{
+		if (value > 0)
+			_posSeries[bar] = value;
+		else
+			_negSeries[bar] = value;
+	}
+
+	private decimal LastValue(int bar)
+	{
+		if(bar == 0)
+			return
+				_posSeries[bar] != 0
+					? _posSeries[bar]
+					: _negSeries[bar];
+
+        return
+			_posSeries[bar - 1] != 0
+				? _posSeries[bar - 1]
+				: _negSeries[bar - 1];
+	}
+
+	#endregion
 }
