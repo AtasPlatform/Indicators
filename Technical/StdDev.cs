fafba3951a7e565536c02e70cb14989b3e3a798d
VladimirPavlenko207 <paradize73v@gmail.com>
2023-12-07 15:45:01 +0200

    StdDevDescription) added.

--- Technical/StdDev.cs
+++ Technical/StdDev.cs
@@ -6,11 +6,10 @@ namespace ATAS.Indicators.Technical
 
 	using OFT.Attributes;
     using OFT.Localization;
-    using Utils.Common.Localization;
 
 	[DisplayName("Standard Deviation")]
-	[LocalizedDescription(typeof(Strings), nameof(Strings.StdDev))]
-	[HelpLink("https://support.atas.net/knowledge-bases/2/articles/7208-stddev")]
+    [Display(ResourceType = typeof(Strings), Description = nameof(Strings.StdDevDescription))]
+    [HelpLink("https://help.atas.net/en/support/solutions/articles/72000602477")]
 	public class StdDev : Indicator
 	{
 		#region Fields
@@ -24,8 +23,9 @@ namespace ATAS.Indicators.Technical
 		[Parameter]
 		[Display(ResourceType = typeof(Strings),
 			Name = nameof(Strings.Period),
-			GroupName = nameof(Strings.Common),
-			Order = 20)]
+			GroupName = nameof(Strings.Settings),
+            Description = nameof(Strings.PeriodDescription),
+            Order = 20)]
 		[Range(1, 10000)]
 		public int Period
 		{
