d7d3e6c8bb13fe9e20697e9bc7b8e1d4d360ac0c
VladimirPavlenko207 <paradize73v@gmail.com>
2023-12-12 13:50:32 +0200

    ZLEMAIndDescription added.

--- Technical/ZLEMA.cs
+++ Technical/ZLEMA.cs
@@ -8,7 +8,8 @@
     using OFT.Localization;
 
     [DisplayName("Zero Lag Exponential Moving Average")]
-	[HelpLink("https://support.atas.net/knowledge-bases/2/articles/45287-zero-lag-exponential-moving-average")]
+    [Display(ResourceType = typeof(Strings), Description = nameof(Strings.ZLEMAIndDescription))]
+    [HelpLink("https://help.atas.net/en/support/solutions/articles/72000602640")]
 	public class ZLEMA : Indicator
 	{
 		#region Fields
@@ -21,7 +22,7 @@
         #region Properties
 
         [Parameter]
-        [Display(ResourceType = typeof(Strings), Name = nameof(Strings.Period), GroupName = nameof(Strings.Settings), Order = 100)]
+        [Display(ResourceType = typeof(Strings), Name = nameof(Strings.Period), GroupName = nameof(Strings.Settings), Description = nameof(Strings.PeriodDescription),Order = 100)]
 		[Range(1, 10000)]
 		public int Period
 		{
