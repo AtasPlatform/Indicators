a5553b0d8586637cdc0da9a51c55817b93d00a73
Rinas Andrey <rinasandrey@gmail.com>
2024-08-07 11:22:37 +0100

    TradesOnChart. Final fixes to release PLAT-1654

--- Technical/TradesOnChart.cs
+++ Technical/TradesOnChart.cs
@@ -10,11 +10,13 @@ using ATAS.DataFeedsCore;
 using OFT.Attributes;
 using OFT.Localization;
 using OFT.Rendering.Context;
+using OFT.Rendering.Tools;
+
 using Color = System.Drawing.Color;
 using DashStyle = System.Drawing.Drawing2D.DashStyle;
 using Pen = OFT.Rendering.Tools.RenderPen;
 
-[FeatureId("NotReady")]
+[Category("Trading")]
 [DisplayName("Trades On Chart")]
 public class TradesOnChart : Indicator
 {
@@ -27,14 +29,35 @@ public class TradesOnChart : Indicator
         internal int CloseBar { get; set; }
         internal decimal ClosePrice { get; set; }
         internal OrderDirections Direction { get; set; }
+		internal decimal PnL { get; set; }
+		internal decimal PnLTicks { get; set; }
+		internal DateTime OpenTime { get; set; }
+		internal DateTime CloseTime { get; set; }
+        internal decimal Volume { get; set; }
+        internal string Security { get; set; }
+
+
+		public TradeObj(HistoryMyTrade trade)
+		{
+			OpenPrice = trade.OpenPrice;
+			ClosePrice = trade.ClosePrice;
+			Direction = trade.OpenVolume > 0 ? OrderDirections.Buy : OrderDirections.Sell;
+			PnL = trade.PnL;
+			PnLTicks = trade.TicksPnL;
+			OpenTime = trade.OpenTime;
+			CloseTime = trade.CloseTime;
+			Volume = Math.Abs(trade.OpenVolume);
+			Security = trade.Security.Code;
+		}
     }
 
     #endregion
 
     #region Fields
 
+    private RenderFont _font = new RenderFont("Arial", 10F, FontStyle.Regular, GraphicsUnit.Point, 204);
+    private RenderStringFormat _stringFormat = new RenderStringFormat() { Alignment = StringAlignment.Near, LineAlignment = StringAlignment.Center };
     private readonly List<TradeObj> _trades = new();
-
     private Pen _buyPen;
     private Pen _sellPen;
     private Color _buyColor;
@@ -46,8 +69,8 @@ public class TradesOnChart : Indicator
 
     #region Properties
 
-    [Display(ResourceType = typeof(Strings), Name = nameof(Strings.Show), GroupName = nameof(Strings.Visualization))]
-    public bool ShowTrades { get; set; } = true;
+    [Display(ResourceType = typeof(Strings), Name = nameof(Strings.ShowLines), GroupName = nameof(Strings.Visualization))]
+    public bool ShowLine { get; set; } = true;
 
     [Display(ResourceType = typeof(Strings), Name = nameof(Strings.BuyColor), GroupName = nameof(Strings.Visualization))]
     public Color BuyColor 
@@ -121,6 +144,12 @@ public class TradesOnChart : Indicator
     protected override void OnInitialize()
     {
         TradingStatisticsProvider.Realtime.HistoryMyTrades.Added += OnTradeAdded;
+        TradingManager.PortfolioSelected += TradingManager_PortfolioSelected;
+    }
+
+    private void TradingManager_PortfolioSelected(Portfolio obj)
+    {
+	    OnRecalculate();
     }
 
     protected override void OnApplyDefaultColors()
@@ -145,13 +174,80 @@ public class TradesOnChart : Indicator
        
     }
 
+    #region Rendering
+
     protected override void OnRender(RenderContext context, DrawingLayouts layout)
     {
-       if(ChartInfo is null) return;
+        if (ChartInfo is null) return;
 
-        if (ShowTrades)
-            DrawTrades(context);
+        DrawTrades(context);
     }
+
+    private void DrawTrades(RenderContext context)
+    {
+        foreach (var trade in _trades)
+        {
+            if (trade.OpenBar > LastVisibleBarNumber || trade.CloseBar < FirstVisibleBarNumber)
+                continue;
+
+            var x1 = ChartInfo.GetXByBar(trade.OpenBar, false);
+            var y1 = ChartInfo.GetYByPrice(trade.OpenPrice, false);
+            var x2 = ChartInfo.GetXByBar(trade.CloseBar, false);
+            var y2 = ChartInfo.GetYByPrice(trade.ClosePrice, false);
+            var pen = GetPenByDirection(trade.Direction);
+
+            if(ShowLine)
+				context.DrawLine(pen, x1, y1, x2, y2);
+
+            var mouseOver = DrawMarker(context, new Point(x1, y1), trade.Direction, true);
+
+            var mouseOver2 = DrawMarker(context, new Point(x2, y2), trade.Direction, false);
+
+            if (mouseOver || mouseOver2)
+            {
+                var cl = trade.PnL > 0 ? _buyColor : _sellColor;
+
+                var text = (trade.Direction == OrderDirections.Buy ? "Long" : "Short") + " " +
+                    trade.Volume.ToString() + " " + trade.Security + Environment.NewLine + Environment.NewLine;
+
+                text += $"Entry\t:  {ChartInfo.GetPriceString(trade.OpenPrice)}, time {trade.OpenTime:dd MMM HH:mm:ss}{Environment.NewLine}";
+                text += $"Exit\t:  {ChartInfo.GetPriceString(trade.ClosePrice)}, time {trade.CloseTime:dd MMM HH:mm:ss}{Environment.NewLine}{Environment.NewLine}";
+                text += $"Result\t: {(trade.PnL > 0 ? "+" : "")}{trade.PnL} ({trade.PnLTicks} ticks)";
+
+                var size = context.MeasureString(Strings.TextTxt, _font);
+                size = new Size(size.Width + 280, size.Height + 110);
+                var rectangle = new Rectangle(MouseLocationInfo.LastPosition, size);
+                context.FillRectangle(cl, rectangle, 10);
+                rectangle.X += 10;
+                context.DrawString(text, _font, Color.AliceBlue, rectangle, _stringFormat);
+            }
+        }
+    }
+
+    private bool DrawMarker(RenderContext context, Point point, OrderDirections direction, bool isOpen)
+    {
+        var shift = MarkerSize * 4;
+        var dir = direction == OrderDirections.Buy ? 1 : -1;
+        var y2 = isOpen ? (point.Y + shift * dir) : (point.Y + shift * (-dir));
+        var point2 = new Point(point.X - shift, y2);
+        var point3 = new Point(point2.X + shift * 2, point2.Y);
+        var color = GetMarkerColor(direction, isOpen);
+
+        var points = new Point[] { point, point2, point3 };
+
+        context.FillPolygon(color, points);
+
+        context.DrawPolygon(ChartInfo.ColorsStore.Grid, points);
+
+        if (IsPointInTriangle(MouseLocationInfo.LastPosition, point, point2, point3))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    #endregion
 
     #endregion
 
@@ -159,19 +255,20 @@ public class TradesOnChart : Indicator
 
     private void AddHistoryMyTrade()
     {
-        var allTrades = TradingStatisticsProvider?.Realtime?.HistoryMyTrades
-                       .Where(t => t.AccountID == TradingManager.Portfolio.AccountID
-                                && t.Security.Instrument == TradingManager.Security.Instrument);
+	    var allTrades = TradingStatisticsProvider?.Realtime?.HistoryMyTrades
+		    .Where(t => t.AccountID == TradingManager.Portfolio.AccountID
+			    && t.Security.Instrument == TradingManager.Security.Instrument);
 
-        foreach (var trade in allTrades)
-        {
-            CreateTradePair(trade); 
-        }
+	    foreach (var trade in allTrades)
+	    {
+		    CreateTradePair(trade);
+	    }
     }
 
     private void OnTradeAdded(HistoryMyTrade trade)
     {
-        CreateTradePair(trade);
+	    if (trade.AccountID == TradingManager.Portfolio.AccountID && trade.Security.Instrument == TradingManager.Security.Instrument)
+		    CreateTradePair(trade);
     }
 
     private void CreateTradePair(HistoryMyTrade trade)
@@ -182,13 +279,10 @@ public class TradesOnChart : Indicator
 
         var exitBar = GetBarByTime(trade.CloseTime);
 
-        var tradeObj = new TradeObj
+        var tradeObj = new TradeObj(trade)
         {
             OpenBar = enterBar,
-            OpenPrice = trade.OpenPrice,
             CloseBar = exitBar,
-            ClosePrice = trade.ClosePrice,
-            Direction = trade.OpenVolume > 0 ? OrderDirections.Buy : OrderDirections.Sell,
         };
 
         _trades.Add(tradeObj);
@@ -207,35 +301,19 @@ public class TradesOnChart : Indicator
         return -1;
     }
 
-    private void DrawTrades(RenderContext context)
+    private bool IsPointInTriangle(Point p, Point p0, Point p1, Point p2)
     {
-        foreach (var trade in _trades)
-        {
-            if (trade.OpenBar > LastVisibleBarNumber || trade.CloseBar < FirstVisibleBarNumber)
-                continue;
+	    double area = TriangleArea(p0, p1, p2);
+	    double area1 = TriangleArea(p, p0, p1);
+	    double area2 = TriangleArea(p, p1, p2);
+	    double area3 = TriangleArea(p, p2, p0);
 
-            var x1 = ChartInfo.GetXByBar(trade.OpenBar, false);
-            var y1=ChartInfo.GetYByPrice(trade.OpenPrice, false);
-            var x2 = ChartInfo.GetXByBar(trade.CloseBar, false);
-            var y2 = ChartInfo.GetYByPrice(trade.ClosePrice, false);
-            var pen = GetPenByDirection(trade.Direction);
-
-            context.DrawLine(pen, x1, y1, x2, y2);
-            DrawMarker(context, new Point(x1, y1), trade.Direction, true);
-            DrawMarker(context, new Point(x2, y2), trade.Direction, false);
-        }
+	    return Math.Abs(area - (area1 + area2 + area3)) < 0.001;
     }
 
-    private void DrawMarker(RenderContext context, Point point, OrderDirections direction, bool isOpen)
+    private double TriangleArea(Point p0, Point p1, Point p2)
     {
-        var shift = MarkerSize * 5;
-        var dir = direction == OrderDirections.Buy ? 1 : -1;
-        var y2 = isOpen ? (point.Y + shift * dir) : (point.Y + shift * (-dir));
-        var point2 = new Point(point.X - shift, y2);
-        var point3 = new Point(point2.X + shift * 2, point2.Y);
-        var color = GetMarkerColor(direction, isOpen);
-
-        context.FillPolygon(color, new Point[] { point, point2, point3 });
+	    return Math.Abs((p0.X * (p1.Y - p2.Y) + p1.X * (p2.Y - p0.Y) + p2.X * (p0.Y - p1.Y)) / 2.0);
     }
 
     private Color GetMarkerColor(OrderDirections direction, bool isOpen)
