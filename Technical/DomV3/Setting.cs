956fae00cb4e034d1f6b53bb4677571059237897
Rinas Andrey <rinasandrey@gmail.com>
2024-08-14 10:30:17 +0100

    PLAT-1675. MBO Dom localization

--- Technical/DomV3/Setting.cs
+++ Technical/DomV3/Setting.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using ATAS.Indicators;
 
 using OFT.Attributes.Editors;
+using OFT.Localization;
 
 public partial class MainIndicator
 {
@@ -22,35 +23,31 @@ public partial class MainIndicator
 
     #region Properties
 
-    #region Aggregation settings
+    #region Colors
 
-    [Display(Name = "Show Volume Sum", Order = 10, GroupName = "Aggregation Setting")]
-    public bool ShowSum
+    [Display(ResourceType = typeof(Strings), Name = nameof(Strings.Bids), GroupName = nameof(Strings.Colors),Order = 2)]
+    public Color BidBlockColor
     {
+	    get => _bidColor;
 	    set
 	    {
-		    _showSum = value;
+		    _bidColor = value;
 		    RedrawChart(_emptyRedrawArg);
 	    }
-	    get => _showSum;
     }
 
-    [Display(Name = "Show Order Count", Order = 20, GroupName = "Aggregation Setting")]
-    public bool ShowCount
+    [Display(ResourceType = typeof(Strings), Name = nameof(Strings.Asks), GroupName = nameof(Strings.Colors), Order = 4)]
+    public Color AskBlockColor
     {
+	    get => _askColor;
 	    set
 	    {
-		    _showCount = value;
+		    _askColor = value;
 		    RedrawChart(_emptyRedrawArg);
 	    }
-	    get => _showCount;
     }
 
-    #endregion
-
-    #region Text
-
-    [Display(Name = "Color", GroupName = "Text", Order = 110)]
+    [Display(ResourceType = typeof(Strings), Name = nameof(Strings.Text), GroupName = nameof(Strings.Colors), Order = 6)]
     public Color TextColor
     {
 	    get => _textColor;
@@ -63,48 +60,49 @@ public partial class MainIndicator
 
     #endregion
 
-    #region Colors
-
-    [Display(Name = "Bid Color", GroupName = "Colors", Order = 200)]
-    public Color BidBlockColor
-    {
-	    get => _bidColor;
-	    set
-	    {
-		    _bidColor = value;
-		    RedrawChart(_emptyRedrawArg);
-	    }
-    }
-
-    [Display(Name = "Ask Color", GroupName = "Colors", Order = 210)]
-    public Color AskBlockColor
-    {
-	    get => _askColor;
-	    set
-	    {
-		    _askColor = value;
-		    RedrawChart(_emptyRedrawArg);
-	    }
-    }
-
-    #endregion
-
     #region Filters
 
     [NumericEditor(0, EditorType = NumericEditorTypes.Spin, Step = 1, DisplayFormat = "F0")]
-    [Display(Name = "Order Size Filter", GroupName = "Filters", Order = 300)]
+    [Display(ResourceType = typeof(Strings), Name = nameof(Strings.ColorFilter), GroupName = nameof(Strings.MBOFilters), Order = 100)]
     public FilterInt OrderSizeFilter { set; get; } = new() { Enabled = true, Value = 0 };
 
     [NumericEditor(0, EditorType = NumericEditorTypes.Spin, Step = 1, DisplayFormat = "F0")]
-    [Display(Name = "Min Block Size", GroupName = "Filters", Order = 310)]
+    [Display(ResourceType = typeof(Strings), Name = nameof(Strings.TotalVolumeFilter), GroupName = nameof(Strings.MBOFilters), Order = 110)]
     public FilterInt MinBlockSize { set; get; } = new() { Enabled = true, Value = 1 };
 
+    #endregion
+
+    #region Aggregation settings
+
+    [Display(ResourceType = typeof(Strings), Name = nameof(Strings.ShowVolume), GroupName = nameof(Strings.Summary), Order = 210)]
+    public bool ShowSum
+    {
+	    set
+	    {
+		    _showSum = value;
+		    RedrawChart(_emptyRedrawArg);
+	    }
+	    get => _showSum;
+    }
+
+    [Display(ResourceType = typeof(Strings), Name = nameof(Strings.ShowOrdersCount), GroupName = nameof(Strings.Summary), Order = 220)]
+    public bool ShowCount
+    {
+	    set
+	    {
+		    _showCount = value;
+		    RedrawChart(_emptyRedrawArg);
+	    }
+	    get => _showCount;
+    }
+
     [NumericEditor(0, EditorType = NumericEditorTypes.Spin, Step = 1, DisplayFormat = "F0")]
-    [Display(Name = "Row Order Count ", GroupName = "Filters", Order = 320)]
+
+    [Display(ResourceType = typeof(Strings), Name = nameof(Strings.OrdersCountFilter), GroupName = nameof(Strings.Summary), Order = 230)]
     public FilterInt RowOrderCount { set; get; } = new() { Enabled = true, Value = 1 };
 
     [NumericEditor(0, EditorType = NumericEditorTypes.Spin, Step = 1, DisplayFormat = "F0")]
-    [Display(Name = "Row Order Volume ", GroupName = "Filters", Order = 330)]
+    [Display(ResourceType = typeof(Strings), Name = nameof(Strings.TotalVolumeFilter), GroupName = nameof(Strings.Summary), Order = 240)]
     public FilterInt RowOrderVolume { set; get; } = new() { Enabled = true, Value = 1 };
 
     #endregion
